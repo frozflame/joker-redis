6886209ac7380dc122674227a4a84106de41fbf5
frozflame <frozflame@outlook.com>
2021-11-27 09:52:52 +0800

    move RepositoryInterface

--- joker/interfaces/filesys/__init__.py
+++ joker/interfaces/filesys/__init__.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 from joker.interfaces.filesys.archives import Archive, TarArchive, ZipArchive
+from joker.interfaces.filesys.repos import RepositoryInterface
 from joker.interfaces.filesys.files import (
     FileStorageInterface, b64_encode_data_url, b64_encode_local_file,
 )
